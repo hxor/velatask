5f116598587e78c44a3d99915cee26cf548a40c3
Kiddie <hakimsembiring@outlook.co.id>
2018-10-26 22:00:04 +0700

    Install NPM and Use Sample Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component('task-component', require('./components/TaskComponent.vue'));
+
 const app = new Vue({
     el: '#app'
 });
