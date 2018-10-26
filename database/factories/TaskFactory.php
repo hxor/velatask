6d4dc2d9fc576ed7169b9c36d7b3b5cabca54db9
Kiddie <hakimsembiring@outlook.co.id>
2018-10-26 21:42:01 +0700

    Create Model, Migration, Controller, Seed and Factory Task

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$prior = ['low', 'medium', 'high'];
+
+$factory->define(App\Task::class, function (Faker $faker) use ($prior) {
+    return [
+        'title' => $faker->sentence(2),
+        'prior' => $prior[mt_rand(0, count($prior) - 1)],
+        'user_id' => 1
+    ];
+});
